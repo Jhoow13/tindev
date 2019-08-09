f1b6a991da59e55f0286c480ae89f84c8ed4a599
Joel Silva <joelcsilva@outlook.com>
2019-08-08 23:41:25 -0300

    cors fix

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 
 mongoose.connect('mongodb+srv://joel:omnistack@cluster0-jqq22.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser: true});
-server.use(cors);
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
